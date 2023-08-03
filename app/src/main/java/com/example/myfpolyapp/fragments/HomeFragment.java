6255ae6163a5893fd4ad50860acb940ffb98d673
tonynhox <anhhuytran1470@gmail.com>
2023-08-03 13:41:02 +0700

    item

---
+++ app/src/main/java/com/example/myfpolyapp/fragments/HomeFragment.java
@@ -0,0 +1,29 @@
+package com.example.myfpolyapp.fragments;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myfpolyapp.R;
+
+public class HomeFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+        return view;
+    }
+
+
+
+}
