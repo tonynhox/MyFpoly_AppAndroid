d37abb078d8ded7d94d0b52399c0e8d7a46e27cc
TrongTre07 <trongtruth2003@gmail.com>
2023-08-03 15:02:50 +0700

    up

--- app/src/main/java/com/example/myfpolyapp/MainActivity.java
+++ app/src/main/java/com/example/myfpolyapp/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.myfpolyapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.widget.Switch;
 
 import com.example.myfpolyapp.fragments.LibraryFrament;
 import com.example.myfpolyapp.fragments.NotifiFragment;
@@ -17,32 +21,36 @@ import com.example.myfpolyapp.fragments.ScheduleFragment;
 import com.example.myfpolyapp.fragments.ScoreFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BottomNavigationView bottomNavigationView;
+    BottomNavigationView bottomNavigationView;
+    NavigationView navigationView;
+    FragmentManager fragmentManager;
+    Fragment fragment;
 
+    DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Toolbar toolBar = findViewById(R.id.toolBar);
-        setSupportActionBar(toolBar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        drawer = findViewById(R.id.drawerLayout);
 
         //Hiển thị mặc định NotifiFragment
         NotifiFragment notifiFragment = new NotifiFragment();
         FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frameLayout, notifiFragment);
-        fragmentTransaction.commit();
 
+        fragmentManager = getSupportFragmentManager();
+        toolBar.setTitle("Thông báo");
+        fragment = new NotifiFragment();
+        fragmentManager.beginTransaction().replace(R.id.frameLayout,fragment).commit();
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment fragment;
                 if (item.getItemId() == R.id.notificationBottom) {
                     toolBar.setTitle("Thông báo");
                     fragment = new NotifiFragment();
@@ -64,6 +72,35 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        navigationView = findViewById(R.id.navigationView);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.icHome) {
+                    bottomNavigationView.setSelectedItemId(R.id.notificationBottom);
+
+                }
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.frameLayout, fragment).commit();
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                if (item.getItemId() == R.id.notificationBottom) {
+//                    toolBar.setTitle("Thông báo");
+//                    fragment = new NotifiFragment();
+//                }
+//                getSupportFragmentManager().beginTransaction()
+//                        .replace(R.id.frameLayout, fragment).commit();
+//                drawer.closeDrawer(GravityCompat.START);
+//                return true;
+//            }
+//        });
 
 //    private void loadFragment(Fragment fragment) {
 //        FragmentManager fragmentManager = getSupportFragmentManager();
